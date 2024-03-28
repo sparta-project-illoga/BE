ca4b92e90d235df91f184e552c9812390c89fd3c
wanhee27 <chowh727@gmail.com>
2024-03-28 16:50:36 +0900

    +init commit 프로젝트 초기 셋팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,55 @@ import { PlanModule } from './plan/plan.module';
 import { TravelModule } from './travel/travel.module';
 import { CategoryModule } from './category/category.module';
 import { LocalModule } from './local/local.module';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import Joi from 'joi';
+import { User } from './user/entities/user.entity';
+import { Travel } from './travel/entities/travel.entity';
+import { Plan } from './plan/entities/plan.entity';
+import { Local } from './local/entities/local.entity';
+import { Category } from './category/entities/category.entity';
+
+const typeOrmModuleOptions = {
+  useFactory: async (
+    configService: ConfigService,
+  ): Promise<TypeOrmModuleOptions> => ({
+    namingStrategy: new SnakeNamingStrategy(),
+    type: 'mysql',
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    host: configService.get('DB_HOST'),
+    port: configService.get('DB_PORT'),
+    database: configService.get('DB_NAME'),
+    entities: [User, Travel, Plan, Local, Category], // 엔티티는 반드시 여기에 명시!
+    synchronize: configService.get('DB_SYNC'),
+    logging: true,
+  }),
+  inject: [ConfigService],
+};
 
 @Module({
-  imports: [UserModule, PlanModule, TravelModule, CategoryModule, LocalModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        JWT_SECRET_KEY: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_NAME: Joi.string().required(),
+        DB_SYNC: Joi.boolean().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync(typeOrmModuleOptions),
+    UserModule,
+    PlanModule,
+    TravelModule,
+    CategoryModule,
+    LocalModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
