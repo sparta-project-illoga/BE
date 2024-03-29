a93a21030c57b0a923d09f22e7f171b518334336
wanhee27 <chowh727@gmail.com>
2024-03-29 21:48:52 +0900

    Feat: post DTO 설정

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,1 +1,15 @@
-export class CreatePostDto {}
+import { Column } from 'typeorm';
+
+export class CreatePostDto {
+  @Column({ type: 'int', nullable: false })
+  userId: number;
+
+  @Column({ type: 'varchar', nullable: false })
+  title: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  content: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  image: string;
+}
