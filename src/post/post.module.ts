0ea281c294283b5bc8d6ad4b2246dc8277fe02dc
wanhee27 <chowh727@gmail.com>
2024-03-29 21:49:52 +0900

    Feat: post CRUD 구현

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,10 +1,12 @@
-import { Module, Post } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { Post } from './entities/post.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post])],
+  imports: [TypeOrmModule.forFeature([Post, User])],
   controllers: [PostController],
   providers: [PostService],
 })
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,26 +1,90 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Post } from './entities/post.entity';
+import { User } from 'src/user/entities/user.entity';
 
 @Injectable()
 export class PostService {
-  createPost(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+  constructor(
+    @InjectRepository(Post)
+    private readonly postRepository: Repository<Post>,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  // 게시물 생성
+  async createPost(user: User, createPostDto: CreatePostDto) {
+    const users = await this.userRepository.findOne({ where: { id: user.id } });
+    if (!users) {
+      throw new BadRequestException('사용자를 찾을 수 없습니다.');
+    }
+
+    const newPost = new Post();
+    newPost.title = createPostDto.title;
+    newPost.content = createPostDto.content;
+    newPost.image = createPostDto.image;
+    newPost.user = users;
+    const post = await this.postRepository.save(newPost);
+    await this.postRepository.save({
+      userId: user.id,
+      postId: post.id,
+    });
+    return post;
   }
 
-  findAllPost() {
-    return `This action returns all post`;
+  // 게시물 전체 조회
+  async findAllPost() {
+    const posts = await this.postRepository.find();
+    return posts;
   }
 
-  findOnePostByPostId(id: number) {
-    return `This action returns a #${id} post`;
+  // 게시물 단건 조회
+  async findOnePostByPostId(postId: number) {
+    const post = await this.postRepository.findOne({ where: { id: postId } });
+    if (!post) {
+      throw new NotFoundException('게시물을 찾을 수 없습니다.');
+    }
+    return post;
   }
 
-  // updatePost(id: number, updatePostDto: UpdatePostDto, userId: number) {
-  //   return `This action updates a #${id} post`;
-  // }
+  // 게시물 수정
+  async updatePost(postId: number, updatePostDto: UpdatePostDto, user: User) {
+    const post = await this.findOnePostByPostId(postId);
+    if (!post) {
+      throw new NotFoundException('게시물을 찾을 수 없습니다.');
+    }
+    if (post.userId !== user.id) {
+      throw new BadRequestException('작성자만 수정할 수 있습니다.');
+    }
+    const isChangedData =
+      updatePostDto.userId !== undefined ||
+      updatePostDto.title !== undefined ||
+      updatePostDto.content !== undefined ||
+      updatePostDto.image !== undefined;
+    if (!isChangedData) {
+      throw new BadRequestException('변경된 내용이 없습니다.');
+    }
+    await this.postRepository.update({ id: postId }, updatePostDto);
+    return { message: '게시물이 수정되었습니다.' };
+  }
 
-  removePost(id: number) {
-    return `This action removes a #${id} post`;
+  // 게시물 삭제
+  async removePost(user: User, postId: number) {
+    const post = await this.findOnePostByPostId(postId);
+    if (!post) {
+      throw new NotFoundException('게시물을 찾을 수 없습니다.');
+    }
+    if (post.userId !== user.id) {
+      throw new BadRequestException('작성자만 삭제할 수 있습니다.');
+    }
+    await this.postRepository.remove(post);
+    return { message: '게시물이 삭제되었습니다.' };
   }
 }
